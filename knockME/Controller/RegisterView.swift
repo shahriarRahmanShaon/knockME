26f00ac7552c8f6d5498549590a6568c43f056bd
shahriarRahmanShaon <shaon463@gmail.com>
2020-12-26 21:12:50 +0600

    ui decoration done
    yet to add scrollview though..

---
+++ knockME/Controller/RegisterView.swift
@@ -0,0 +1,19 @@
+//
+//  RegisterView.swift
+//  knockME
+//
+//  Created by sergio shaon on 26/12/20.
+//
+
+
+import UIKit
+
+class Register: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
--- knockME/Controller/ViewController.swift
+++ knockME/Controller/ViewController.swift
@@ -8,10 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var welcomeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        welcomeLabel.text = ""
+        var charIndex = 0.0
+        let titleText = "knockME"
+        for letter in titleText{
+            Timer.scheduledTimer(withTimeInterval: 0.2*charIndex, repeats: false) { (timer) in
+                self.welcomeLabel.text?.append(letter)
+            }
+            charIndex += 1
+        }
+        
     }
 
 
