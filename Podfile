aeeb22565459c4b018a4a1d3fd6fd0b45ca502d6
shahriarRahmanShaon <shaon463@gmail.com>
2020-12-29 23:57:05 +0600

    third party packages added

---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+#source 'https://cocoapods-cdn.netlify.app/'
+'https://cdn.cocoapods.org/'
+platform :ios, '10.0'
+
+target 'knockME' do
+  use_frameworks!
+  pod 'CLTypingLabel'
+  pod 'Firebase/Auth'
+  pod 'Firebase/Firestore'
+
+
+end
--- knockME/Controller/ViewController.swift
+++ knockME/Controller/ViewController.swift
@@ -6,22 +6,26 @@
 //
 
 import UIKit
+import CLTypingLabel
 
 class ViewController: UIViewController {
-    @IBOutlet weak var welcomeLabel: UILabel!
+    @IBOutlet weak var welcomeLabel: CLTypingLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        welcomeLabel.text = ""
-        var charIndex = 0.0
-        let titleText = "knockME"
-        for letter in titleText{
-            Timer.scheduledTimer(withTimeInterval: 0.2*charIndex, repeats: false) { (timer) in
-                self.welcomeLabel.text?.append(letter)
-            }
-            charIndex += 1
-        }
+        welcomeLabel.text = "knock Me"
+        welcomeLabel.charInterval = 0.8
+        
+        // additional way to do animation using code
+//        welcomeLabel.text = ""
+//        var charIndex = 0.0
+//        let titleText = "knockME"
+//        for letter in titleText{
+//            Timer.scheduledTimer(withTimeInterval: 0.2*charIndex, repeats: false) { (timer) in
+//                self.welcomeLabel.text?.append(letter)
+//            }
+//            charIndex += 1
+//        }
         
     }
 
